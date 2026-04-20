eeb4ea12eedc267b46b5c2c7728fcb89e61fd61b
Gaurav Ray <gaurav.sinharay@swansea.ac.uk>
2026-04-20 20:08:34 +0100

    sets up new module via add_module_template.sh

---
+++ Hadrons/Modules/MIO/LoadIldg.cpp
@@ -0,0 +1,7 @@
+#include <Hadrons/Modules/MIO/LoadIldg.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MIO;
+
+template class HADRONS_NAMESPACE::MIO::TLoadIldg<FIMPL>;
---
+++ Hadrons/Modules/MIO/LoadIldg.hpp
@@ -0,0 +1,85 @@
+#ifndef Hadrons_MIO_LoadIldg_hpp_
+#define Hadrons_MIO_LoadIldg_hpp_
+
+#include <Hadrons/Global.hpp>
+#include <Hadrons/Module.hpp>
+#include <Hadrons/ModuleFactory.hpp>
+
+BEGIN_HADRONS_NAMESPACE
+
+/******************************************************************************
+ *                         LoadIldg                                 *
+ ******************************************************************************/
+BEGIN_MODULE_NAMESPACE(MIO)
+
+class LoadIldgPar: Serializable
+{
+public:
+    GRID_SERIALIZABLE_CLASS_MEMBERS(LoadIldgPar,
+                                    unsigned int, i);
+};
+
+template <typename FImpl>
+class TLoadIldg: public Module<LoadIldgPar>
+{
+public:
+    // constructor
+    TLoadIldg(const std::string name);
+    // destructor
+    virtual ~TLoadIldg(void) {};
+    // dependency relation
+    virtual std::vector<std::string> getInput(void);
+    virtual std::vector<std::string> getOutput(void);
+    // setup
+    virtual void setup(void);
+    // execution
+    virtual void execute(void);
+};
+
+MODULE_REGISTER_TMP(LoadIldg, TLoadIldg<FIMPL>, MIO);
+
+/******************************************************************************
+ *                 TLoadIldg implementation                             *
+ ******************************************************************************/
+// constructor /////////////////////////////////////////////////////////////////
+template <typename FImpl>
+TLoadIldg<FImpl>::TLoadIldg(const std::string name)
+: Module<LoadIldgPar>(name)
+{}
+
+// dependencies/products ///////////////////////////////////////////////////////
+template <typename FImpl>
+std::vector<std::string> TLoadIldg<FImpl>::getInput(void)
+{
+    std::vector<std::string> in;
+    
+    return in;
+}
+
+template <typename FImpl>
+std::vector<std::string> TLoadIldg<FImpl>::getOutput(void)
+{
+    std::vector<std::string> out = {getName()};
+    
+    return out;
+}
+
+// setup ///////////////////////////////////////////////////////////////////////
+template <typename FImpl>
+void TLoadIldg<FImpl>::setup(void)
+{
+    
+}
+
+// execution ///////////////////////////////////////////////////////////////////
+template <typename FImpl>
+void TLoadIldg<FImpl>::execute(void)
+{
+    
+}
+
+END_MODULE_NAMESPACE
+
+END_HADRONS_NAMESPACE
+
+#endif // Hadrons_MIO_LoadIldg_hpp_
